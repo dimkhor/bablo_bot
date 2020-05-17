d0d815f524df084368156b92495410a7c179ec68
dimkhor <35232358+dimkhor@users.noreply.github.com>
2020-05-18 01:23:21 +0300

    Update bablo_bot.py

--- bablo_bot.py
+++ bablo_bot.py
@@ -1,38 +1,18 @@
 """Сервер Telegram бота, запускаемый непосредственно"""
 #import logging
 import os
-os.chdir(r'C:\Users\dmitriy.khoroshenkiy\Desktop\cs\bablo_bot_many_users')
-#import aiohttp
-#from aiogram import Bot, Dispatcher, executor, types
-
 import exceptions
 import expenses
 from categories import Categories
 import  graphics
 import datetime
-#from middlewares import AccessMiddleware
-
-
-#logging.basicConfig(level=logging.INFO)
-
-#API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
-#PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
-#PROXY_AUTH = aiohttp.BasicAuth(
-#    login=os.getenv("TELEGRAM_PROXY_LOGIN"),
-#    password=os.getenv("TELEGRAM_PROXY_PASSWORD")
-#)
-#ACCESS_ID = os.getenv("TELEGRAM_ACCESS_ID")
-
-#bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
-#dp = Dispatcher(bot)
-#dp.middleware.setup(AccessMiddleware(ACCESS_ID))
-#-----------------------------------------------#
-TOKEN='1073786035:AAHVEc9fimGKWASp8WJngQFfzEKBWpeFZl8'
-PROXY_URL='http://193.232.150.42:3128'
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+#logging.basicConfig(level=logging.INFO)
 
+TOKEN=os.getenv("TELEGRAM_API_TOKEN")
+PROXY_URL=os.getenv("TELEGRAM_PROXY_URL")
 bot = Bot(token=TOKEN,proxy=PROXY_URL)
 dp = Dispatcher(bot)
 
